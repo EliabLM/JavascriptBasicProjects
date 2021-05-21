75821383c9bf2227b7b506de23220f72ed7f466b
Eliab Lopez <eliablopez@hotmail.com>
2021-05-20 21:54:31 -0500

    add increase and reset button

--- 02_counter/app.js
+++ 02_counter/app.js
@@ -9,8 +9,22 @@ btns.forEach(function (btn) {
         if (styles.contains('decrease')) {
             count--;
         }
-        console.log(count);
+        else if (styles.contains("increase")) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        else if (count < 0) {
+            value.style.color = "red";
+        }
+        else if (count === 0) {
+            value.style.color = "black";
+        }
         value.textContent = count;
-        console.log(value);
     })
 })
