60cc8e6831b2fe21eda3301a1b1fbb9ca93ab6d0
eliablm <eliablopez@hotmail.com>
2021-06-09 19:28:57 -0500

    format document fixed

--- 06_modal/app.js
+++ 06_modal/app.js
@@ -6,12 +6,10 @@ const closeBtn = document.querySelector(".close-btn");
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
-modalBtn.addEventListener('click', function () {
-    modalOverlay.classList.toggle('open-modal');
-})
+modalBtn.addEventListener("click", function () {
+	modalOverlay.classList.toggle("open-modal");
+});
 
 closeBtn.addEventListener("click", function () {
-    modalOverlay.classList.remove("open-modal");
-})
-
-
+	modalOverlay.classList.remove("open-modal");
+});
